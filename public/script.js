7d5fb70450d64cc71909d1a1838f92ad18fee9e2
pupinupi <cool.lg-lg@yandex.ru>
2026-02-26 14:51:05 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,57 +1,48 @@
 const socket = io();
 let roomCode = "";
 
-const cells = [
-"Старт","Москва","Шанс","Кипр","M-shering","Удача",
-"M-taxi","Шаур-Mell","Mellstroy Game","Тюрьма",
-"Mellbank","Mellcoin","KICK","YouTube","Шанс",
-"Гомель","Mell beer","Mell burger","Minecraft",
-"CS GO","Dota 2","Mell berries","Удача","Старт"
-];
-
-function renderBoard(players) {
-
-    const board = document.getElementById("board");
-    board.innerHTML = "";
-
-    cells.forEach((cell, index) => {
-
-        const div = document.createElement("div");
-        div.className = "cell";
-        div.innerHTML = `<p>${cell}</p>`;
-
-        players.forEach(p => {
-            if (p.position === index) {
-                div.innerHTML += `<span>${p.name}</span>`;
-            }
-        });
-
-        board.appendChild(div);
-    });
-}
-
 function join(){
-    const name = document.getElementById("name").value;
-    roomCode = document.getElementById("room").value;
+  const name = document.getElementById("name").value;
+  roomCode = document.getElementById("room").value;
 
-    socket.emit("joinRoom", { name, roomCode });
+  socket.emit("joinRoom", {
+    name,
+    roomCode,
+    color: "red"
+  });
 
-    document.getElementById("join").style.display="none";
-    document.getElementById("game").style.display="block";
+  document.getElementById("join").style.display="none";
+  document.getElementById("game").style.display="block";
 }
 
 function start(){
-    socket.emit("startGame", roomCode);
+  socket.emit("startGame", roomCode);
 }
 
 function roll(){
-    socket.emit("rollDice", roomCode);
+  socket.emit("rollDice", roomCode);
 }
 
-socket.on("roomUpdate", (room)=>{
-    renderBoard(room.players);
+socket.on("updateGame", (game) => {
+  render(game);
 });
 
 socket.on("diceRolled", (data)=>{
-    alert(data.player+" выбросил "+data.dice);
+  alert(data.player + " выбросил " + data.dice);
 });
+
+function render(game){
+  const board = document.getElementById("board");
+  board.innerHTML = "";
+
+  game.players.forEach(p=>{
+    const div = document.createElement("div");
+    div.className="player";
+    div.innerHTML = `
+      <h3>${p.name}</h3>
+      <p>Позиция: ${p.position}</p>
+      <p>Деньги: ${p.money}</p>
+    `;
+    board.appendChild(div);
+  });
+}
